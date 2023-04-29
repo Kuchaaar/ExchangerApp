0663fe8834518de1c40dbc005107d8f433c45d3f
Kuchaaar <kuba_kucharek@wp.pl>
2023-04-29 22:08:51 +0200

    dziala niby

--- src/main/java/com/exchanger/ExchangerApp/currency/integration/CurrencyRepo.java
+++ src/main/java/com/exchanger/ExchangerApp/currency/integration/CurrencyRepo.java
@@ -1,20 +1,19 @@
 package com.exchanger.ExchangerApp.currency.integration;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@Component
 public class CurrencyRepo {
     private final Map<String, Currency> currencyMap = new HashMap<>();
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate ;
-    private static final String insertIntoSql = "INSERT INTO Currency VALUES(?,?,?)";
+    private static final String insertIntoSql = "INSERT INTO Currency (currency,code,mid) VALUES (?,?,?)";
 
     public CurrencyRepo(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -29,9 +28,11 @@ public class CurrencyRepo {
         return this.jdbcTemplate.batchUpdate(insertIntoSql,
                 new BatchPreparedStatementSetter(){
                     public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setString(1,currencyMap.get(i).currency());
-                        ps.setString(2,currencyMap.get(i).code());
-                        ps.setDouble(3,currencyMap.get(i).mid());
+                        List<Currency> currencies = new ArrayList<>(currencyMap.values());
+                        Currency currency = currencies.get(i);
+                        ps.setString(1,currency.currency());
+                        ps.setString(2,currency.code());
+                        ps.setDouble(3,currency.mid());
                     }
                     public int getBatchSize() {
                         return currencyMap.size();
