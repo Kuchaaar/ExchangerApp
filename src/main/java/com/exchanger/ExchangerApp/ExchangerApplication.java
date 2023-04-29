c4a9398859cc5ff96f94bb60ccb05feebcbd3012
Kuchaaar <kuba_kucharek@wp.pl>
2023-04-29 22:11:08 +0200

    poprawka

--- src/main/java/com/exchanger/ExchangerApp/ExchangerApplication.java
+++ src/main/java/com/exchanger/ExchangerApp/ExchangerApplication.java
@@ -1,10 +1,8 @@
 package com.exchanger.ExchangerApp;
 
-import com.exchanger.ExchangerApp.currency.domain.CurrencyDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
--- src/main/java/com/exchanger/ExchangerApp/Runner.java
+++ src/main/java/com/exchanger/ExchangerApp/Runner.java
@@ -1,15 +1,11 @@
 package com.exchanger.ExchangerApp;
 
-import com.exchanger.ExchangerApp.currency.domain.CurrencyDao;
 import com.exchanger.ExchangerApp.currency.domain.CurrencyFetcher;
 import com.exchanger.ExchangerApp.currency.integration.CurrencyClient;
 import com.exchanger.ExchangerApp.currency.integration.CurrencyRepo;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Component
 public class Runner implements ApplicationListener<ApplicationReadyEvent> {
@@ -18,7 +14,7 @@ public class Runner implements ApplicationListener<ApplicationReadyEvent> {
     private final CurrencyRepo currencyRepo;
     private final CurrencyClient currencyClient;
 
-    public Runner(CurrencyFetcher currencyFetcher, CurrencyDao currencyDao, CurrencyRepo currencyRepo, CurrencyClient currencyClient) {
+    public Runner(CurrencyFetcher currencyFetcher, CurrencyRepo currencyRepo, CurrencyClient currencyClient) {
         this.currencyFetcher = currencyFetcher;
         this.currencyRepo = currencyRepo;
         this.currencyClient = currencyClient;
