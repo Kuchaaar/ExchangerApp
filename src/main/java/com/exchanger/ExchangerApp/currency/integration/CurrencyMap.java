a553d10f506c904df8f9915da70f48afa97decba
Kuchaaar <kuba_kucharek@wp.pl>
2023-04-30 13:02:20 +0200

    zmiany

---
+++ src/main/java/com/exchanger/ExchangerApp/currency/integration/CurrencyMap.java
@@ -0,0 +1,7 @@
+package com.exchanger.ExchangerApp.currency.integration;
+
+import java.util.List;
+
+public interface CurrencyMap {
+    public void saveAll(List<CurrenciesResponse> list);
+}
--- src/main/java/com/exchanger/ExchangerApp/currency/integration/CurrencyRepo.java
+++ src/main/java/com/exchanger/ExchangerApp/currency/integration/CurrencyRepo.java
@@ -3,11 +3,9 @@ import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.*;
 @Component
-public class CurrencyRepo {
+public class CurrencyRepo implements CurrencyMap {
     private final Map<String, Currency> currencyMap = new HashMap<>();
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate ;
@@ -18,23 +16,10 @@ public class CurrencyRepo {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public int[] saveAll(List<CurrenciesResponse> list) {
+    public void saveAll(List<CurrenciesResponse> list) {
         list.stream()
                 .map(CurrenciesResponse::rates)
                 .flatMap(Collection::stream)
                 .forEach(currency -> currencyMap.put(currency.code(), currency));
-        return this.jdbcTemplate.batchUpdate(insertIntoSql,
-                new BatchPreparedStatementSetter(){
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        List<Currency> currencies = new ArrayList<>(currencyMap.values());
-                        Currency currency = currencies.get(i);
-                        ps.setString(1,currency.currency());
-                        ps.setString(2,currency.code());
-                        ps.setDouble(3,currency.mid());
-                    }
-                    public int getBatchSize() {
-                        return currencyMap.size();
-                    }
-                });
     }
 }
